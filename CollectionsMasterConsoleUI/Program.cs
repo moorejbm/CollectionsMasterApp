def5d0c6c26d56c063278e72ef06135fd75b27bf
Jason Moore <moorejbm@gmail.com>
2021-01-30 22:49:12 -0600

    UpdateWork

--- CollectionsMasterConsoleUI/Program.cs
+++ CollectionsMasterConsoleUI/Program.cs
@@ -13,18 +13,21 @@ namespace CollectionsMasterConsoleUI
 
             #region Arrays
             // Create an integer Array of size 50
+            var numbers = new int[50];
             
 
             //Create a method to populate the number array with 50 random numbers that are between 0 and 50
-            
+            Populater(numbers);
 
             //Print the first number of the array
+            Console.WriteLine($"{numbers[0]}");
 
             //Print the last number of the array            
+            Console.WriteLine($"{numbers[numbers.Length -1]}");
 
             Console.WriteLine("All Numbers Original");
             //Use this method to print out your numbers from arrays or lists
-            //NumberPrinter();
+            NumberPrinter(numbers);
             Console.WriteLine("-------------------");
 
             //Reverse the contents of the array and then print the array out to the console.
@@ -35,18 +38,23 @@ namespace CollectionsMasterConsoleUI
 
             Console.WriteLine("---------REVERSE CUSTOM------------");
 
+            ReverseArray(numbers);
+
             Console.WriteLine("-------------------");
 
             //Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
             Console.WriteLine("Multiple of three = 0: ");
-            
+
+            ThreeKiller(numbers);
 
             Console.WriteLine("-------------------");
 
             //Sort the array in order now
             /*      Hint: Array.____()      */
             Console.WriteLine("Sorted numbers:");
-            
+                                  
+            Array.Sort(numbers);
+            NumberPrinter(numbers);
 
             Console.WriteLine("\n************End Arrays*************** \n");
             #endregion
@@ -56,10 +64,10 @@ namespace CollectionsMasterConsoleUI
 
             /*   Set Up   */
             //Create an integer List
-            
+            var numList = new List<int>();
 
             //Print the capacity of the list to the console
-            
+            Console.WriteLine($"{numList.Count}");
 
             //Populate the List with 50 random numbers between 0 and 50 you will need a method for this            
             
@@ -101,7 +109,15 @@ namespace CollectionsMasterConsoleUI
 
         private static void ThreeKiller(int[] numbers)
         {
-            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i] % 3 == 0)
+                {
+                    numbers[i] = 0;
+                }
+            }
+
+            NumberPrinter(numbers);
         }
 
         private static void OddKiller(List<int> numberList)
@@ -114,21 +130,31 @@ namespace CollectionsMasterConsoleUI
             
         }
 
-        private static void Populater(List<int> numberList)
+        private static void Populater(List<int> numbers)
         {
+            
+                              
             Random rng = new Random();
 
-        }
+           }
 
         private static void Populater(int[] numbers)
-        {
-            Random rng = new Random();
+            {
+             //Create a method to populate the number array with 50 random numbers that are between 0 and 50
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Random rng = new Random();
+                    numbers[i] = rng.Next(0, 50);
+                }
 
         }        
 
         private static void ReverseArray(int[] array)
         {
-            
+                Array.Reverse(array);
+
+                NumberPrinter(array);
         }
 
         /// <summary>
